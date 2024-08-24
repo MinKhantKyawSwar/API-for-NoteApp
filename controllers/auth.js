354b16357f837715e5a617b3800c9885605bb777
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-08-24 17:59:22 +0630

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,3 +32,33 @@ exports.register = (req, res, next) => {
       res.status(400).json({ message: "Something went wrong" });
     });
 };
+
+exports.login = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errorMessages: errors.array(),
+      });
+    }
+
+    const { email, password } = req.body;
+
+    const userDoc = await User.findOne({ email });
+    if (!userDoc) {
+      return res.status(401).json({ message: "Email does not exist." });
+    }
+
+    const isMatched = bcrypt.compareSync(password, userDoc.password);
+    if (!isMatched) {
+      return res.status(401).json({ message: "Wrong user credentials." });
+    } else {
+      return res.status(200).json({ message: "Login success" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: err.message });
+  }
+};
